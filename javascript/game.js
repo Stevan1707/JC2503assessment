b0fa1ab9992f520d95dc2bd617d1e759f11c9691
Stevan1707 <1553868699@qq.com>
2025-05-26 17:07:51 +0800

    写了点game的东西，大概有个思路了，就是玩家还不知道怎么写

--- javascript/game.js
+++ javascript/game.js
@@ -1,3 +1,16 @@
-const question = "To which country does this flag belong?"
-flags = [1,2,3,4,5]
+
+
 //1:USA  2:UK  3:French  4:Russia  5:Poland
+playerlist=[];
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('sign_in');
+    button.addEventListener('click',这里填写submit);
+});
+
+function submit(){
+    const player = document.getElementById('Username');
+
+}
+
+const question = "To which country does this flag belong?";
